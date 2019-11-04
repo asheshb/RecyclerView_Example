4654df5f7547aeb611820803b49f06d6e901560a
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-11-04 13:51:31 +0530

    change imageview to tools

--- app/src/main/java/com/example/recyclerviewexample/CountryAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexample/CountryAdapter.kt
@@ -7,7 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CountryAdapter: RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+class CountryAdapter: RecyclerView.Adapter<CountryAdapter.CountryViewHolder>(){
 
     var countryData = arrayOf<Country>()
         set(value){
@@ -15,7 +15,7 @@ class CountryAdapter: RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
             notifyDataSetChanged()
         }
 
-    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    class CountryViewHolder (view: View) : RecyclerView.ViewHolder(view) {
         val countryFlag: ImageView = view.findViewById(R.id.country_flag)
         val countryName: TextView = view.findViewById(R.id.country_name)
         val capitalCity: TextView = view.findViewById(R.id.country_capital)
@@ -24,15 +24,15 @@ class CountryAdapter: RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
     override fun getItemCount() = countryData.size
 
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): ViewHolder {
+                                    viewType: Int): CountryViewHolder {
 
         val itemLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.country_item, parent, false)
 
-        return ViewHolder(itemLayout)
+        return CountryViewHolder(itemLayout)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
         with(holder){
             countryFlag.setImageResource(countryData[position].flagId)
             countryName.text = countryData[position].name
